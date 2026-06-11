686b33f717605be729e53a6190157b02773f17d3
NdotBrndL <ndotbrndl@gmail.com>
2026-06-12 02:04:42 +0700

    Create JUMP.lua

---
+++ JUMP.lua
@@ -0,0 +1,6 @@
+local player = game.Players.LocalPlayer
+local humanoid = player.Character or player.CharacterAdded:Wait()
+
+humanoid = humanoid:WaitForChild("Humanoid")
+humanoid.UseJumpPower = true
+humanoid.JumpPower = 130
