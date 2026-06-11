34cbaa136077150fc7b8efa0cae2686e3e85db68
NdotBrndL <ndotbrndl@gmail.com>
2026-06-11 23:30:37 +0700

    Delete Speed.lua

--- Speed.lua
+++
@@ -1,7 +0,0 @@
-local player = game.Players.LocalPlayer
-local character = player.Character or player.CharacterAdded:Wait()
-local humanoid = character:WaitForChild("Humanoid")
-
-humanoid.WalkSpeed = 50
-
-print("Speed aktif")
